84e50ca68a5199cef19c858349873bac6e3898c2
KKK510310 <Idichi.10.Tatsuji@jp.nokgrp.com>
2024-03-18 12:53:54 +0900

    テストコード作成

--- para_porter_client/src/main.rs
+++ para_porter_client/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    let res = reqwest::get("http://127.0.0.1:8000/").await.unwrap().text().await.unwrap();
+    println!("{}",res);
 }
